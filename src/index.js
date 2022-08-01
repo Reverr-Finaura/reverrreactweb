b262974117399c50742a5545d4b4003b4c391dac
Aditya <akaditya394@gmail.com>
2022-08-01 17:20:38 +0530

    google signup implemented

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from 'react-redux';
-// import { store } from './app/store';
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 
@@ -11,8 +11,8 @@ const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./Auth.module.css";
+import { auth } from "../../firebase";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { login } from "../../features/userSlice";
 
 function Auth() {
+  const [userType, setUserType] = useState("founder");
+  const dispatch = useDispatch();
+
+  const provider = new GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((userCredential) => {
+        dispatch(
+          login({
+            email: auth.currentUser.email,
+            uid: auth.currentUser.uid,
+            displayName: auth.currentUser.displayName,
+            profilePic: auth.currentUser.photoURL,
+            userType: userType,
+          })
+        );
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
   return (
-    <div>Auth</div>
-  )
+    <section className={styles.auth}>
+      <div className={styles.signup}>
+        <div>
+          <button onClick={() => setUserType("founder")}>
+            Join as a Founder
+          </button>
+          <button onClick={() => setUserType("mentor")}>
+            Join as a Mentor
+          </button>
+        </div>
+        <div>
+          <h1>Get started as a {userType}</h1>
+        </div>
+        <button onClick={signInWithGoogle}>Sign up with Google</button>
+        <div>
+          <p>Or Sign Up with your E-mail</p>
+        </div>
+        <form>
+          <div className={styles.name}>
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div>
+            <input type="email" placeholder="Your E-Mail" />
+          </div>
+          <div>
+            <input type="password" placeholder="Enter a password" />
+          </div>
+          <div>
+            <input type="password" placeholder="Confirm Password" />
+          </div>
+          <button>Sign Up</button>
+        </form>
+        <p>
+          Already have an account? <button>Login</button>
+        </p>
+        <p>
+          Forgot Password? <button>Click Here</button>
+        </p>
+      </div>
+    </section>
+  );
 }
 
-export default Auth
+export default Auth;
