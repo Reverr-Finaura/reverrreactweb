32258aa2f24189c066012adb4306304b2cd9b331
Nikita <nikitakubavat33@gmail.com>
2022-08-02 20:33:35 +0530

    password-reset done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
+import ForgotPassword from "./pages/Forgotpassword/ForgotPassword";
 import Login from "./pages/Login/Login";
+import PasswordReset from "./pages/PasswordRecover/PasswordReset";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Auth />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
+      <Route path="/password-reset" element={<PasswordReset />} />
     </Routes>
   );
 }
---
+++ src/pages/PasswordRecover/PasswordReset.jsx
@@ -0,0 +1,45 @@
+import { confirmPasswordReset } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { auth } from "../../firebase";
+import styles from "./Passwordreset.module.css";
+
+const PasswordReset = () => {
+  const [newPassword, setNewPassword] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+    const oobCode = searchParams.get("oobCode");
+
+    confirmPasswordReset(auth, oobCode, newPassword)
+      .then(() => {
+        alert("Password changed successfully");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  return (
+    <section className={styles.auth}>
+      <div className={styles.password_reset}>
+        <form onSubmit={handlePasswordChange}>
+          <input
+            type="password"
+            id=""
+            onChange={(e) => {
+              setNewPassword(e.target.value);
+            }}
+            placeholder="Enter your new password"
+          />
+          <p>
+            <Link to="/login">Back to Login</Link>
+          </p>
+          <button type="submit">Save New Password</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default PasswordReset;
