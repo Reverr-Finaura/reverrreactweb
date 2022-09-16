4d70587184cf25001c96fe8656997e7cbdb479ad
Aditya <akaditya394@gmail.com>
2022-09-16 23:49:14 +0530

    mentors fetched

--- src/pages/Mentor/Mentor.jsx
+++ src/pages/Mentor/Mentor.jsx
@@ -7,42 +7,48 @@ import Footer from "../Footer/Footer";
 import Expertise from "../../components/Expertise/Expertise";
 import Price from "../../components/Price/Price";
 import MentorComponent from "../../components/MentorComponent/MentorComponent";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useParams } from "react-router-dom";
 
 function Mentor() {
+  const { clickedOn, type } = useParams();
   const [width, setWidth] = useState(window.innerWidth);
-  const prices = [
-    { id: "1", price: "Rs.499" },
-    { id: "2", price: "Rs.749" },
-    { id: "3", price: "Rs.999" },
-    { id: "4", price: "Rs.999+" },
-  ];
+  const [mentorsArray, setMentorsArray] = useState([]);
+  const data = [];
+  // const prices = [
+  //   { id: "1", price: "Rs.499" },
+  //   { id: "2", price: "Rs.749" },
+  //   { id: "3", price: "Rs.999" },
+  //   { id: "4", price: "Rs.999+" },
+  // ];
 
-  const mentors = [
-    {
-      id: "1",
-      name: "Jimmy Joel",
-      img: "/images/mentorimg1.svg",
-      type: "Market Researcher",
-      rating: "⭐⭐⭐",
-      noofReviews: "76 Reviews",
-    },
-    {
-      id: "2",
-      name: "Jimmy Joel",
-      img: "/images/mentorimg1.svg",
-      type: "Market Researcher",
-      rating: "⭐⭐⭐",
-      noofReviews: "76 Reviews",
-    },
-    {
-      id: "3",
-      name: "Jimmy Joel",
-      img: "/images/mentorimg1.svg",
-      type: "Market Researcher",
-      rating: "⭐⭐⭐",
-      noofReviews: "76 Reviews",
-    },
-  ];
+  // const mentors = [
+  //   {
+  //     id: "1",
+  //     name: "Jimmy Joel",
+  //     img: "/images/mentorimg1.svg",
+  //     type: "Market Researcher",
+  //     rating: "⭐⭐⭐",
+  //     noofReviews: "76 Reviews",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Jimmy Joel",
+  //     img: "/images/mentorimg1.svg",
+  //     type: "Market Researcher",
+  //     rating: "⭐⭐⭐",
+  //     noofReviews: "76 Reviews",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Jimmy Joel",
+  //     img: "/images/mentorimg1.svg",
+  //     type: "Market Researcher",
+  //     rating: "⭐⭐⭐",
+  //     noofReviews: "76 Reviews",
+  //   },
+  // ];
 
   const updateWidth = () => {
     setWidth(window.innerWidth);
@@ -51,6 +57,25 @@ function Mentor() {
   useEffect(() => {
     window.addEventListener("resize", updateWidth);
     return () => window.removeEventListener("resize", updateWidth);
+  }, []);
+
+  useEffect(() => {
+    async function fetchMentorExpertise() {
+      const usersRef = collection(db, "Users");
+      const q = query(
+        usersRef,
+        where("userType", "==", "Mentor"),
+        where(clickedOn, "==", type)
+      );
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        data.push(doc.data());
+      });
+      console.log(data);
+      setMentorsArray(data);
+    }
+
+    fetchMentorExpertise();
   }, []);
   return (
     <>
@@ -108,7 +133,19 @@ function Mentor() {
                 />
               ))}
             </div> */}
-            <div className={styles.mentors}></div>
+            <div className={styles.mentors}>
+              {mentorsArray.map((item, index) => (
+                <MentorComponent
+                  key={index + Math.random()}
+                  name={item?.name}
+                  img={item?.image}
+                  type={item?.domain}
+                  rating={item?.totalRating}
+                  to={item?.email}
+                  // noofReviews={item?.noofReviews}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
